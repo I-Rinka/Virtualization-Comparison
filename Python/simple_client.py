b58a2db310e4f969787cdeb8fa16a032aba19cca
I-Rinka <I_Rinka@outlook.com>
2021-05-05 15:41:03 +0800

    python can run

--- Python/simple_client.py
+++ Python/simple_client.py
@@ -1,16 +1,23 @@
-# -*- coding: UTF-8 -*-
-# 文件名：server.py
- 
-import socket               # 导入 socket 模块
- 
-s = socket.socket()         # 创建 socket 对象
-host = socket.gethostname() # 获取本地主机名
-port = 7786                # 设置端口
-s.bind((host, port))        # 绑定端口
- 
-s.listen(5)                 # 等待客户端连接
-while True:
-    c,addr = s.accept()     # 建立客户端连接
-    print('连接地址：', addr)
-    c.send('欢迎访问菜鸟教程！')
-    c.close()                # 关闭连接
+#!/usr/bin/python3           # This is client.py file
+
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = socket.gethostname()                           
+host="192.168.56.1"
+
+print(host)
+
+port = 9999
+
+# connection to hostname on the port.
+s.connect((host, port))                               
+
+# Receive no more than 1024 bytes
+msg = s.recv(1024)                                     
+
+s.close()
+print (msg.decode('ascii'))
