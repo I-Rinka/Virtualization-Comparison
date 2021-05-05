327d99573bd417dfdfcf11ee826d68f0b700613b
I-Rinka <I_Rinka@outlook.com>
2021-05-05 10:34:27 +0000

    firecracker start up time bench mark down

--- Docker/measure_time.sh
+++ Docker/measure_time.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 start_time=$(date +%s%N)
-end_time=$(sudo docker run ubuntu:bionic /bin/date "+%s%N")
+end_time=$(sudo docker run ubuntu18 /bin/date "+%s%N")
 echo $(($end_time - $start_time)) ns
