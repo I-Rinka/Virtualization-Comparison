4d14dc4b5eabc04669a0f91de0794a7daf30550e
I-Rinka <I_Rinka@outlook.com>
2021-05-05 13:22:11 +0000

    up date startup bench mark

--- Docker/startup_dk_loop.sh
+++ Docker/startup_dk_loop.sh
@@ -2,11 +2,11 @@
 for i in {1..3}
 do
 echo $i
+sudo service docker start
 sudo python3 ./get_docker_start_time.py
 sudo docker container prune -f
 # sleep 100
 sudo ps -aux | grep docker | awk '{print $2}' | sudo xargs kill
-sudo service docker start
 if [ i == 3 ]
     then
         exit 0
