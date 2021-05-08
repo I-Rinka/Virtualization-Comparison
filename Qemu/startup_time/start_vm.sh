a6938dfff6f322e6464192d0bab36d04bbc9341e
I-Rinka <I_Rinka@outlook.com>
2021-05-08 10:56:15 +0000

    startup time measurement OK

--- Qemu/startup_time/start_vm.sh
+++ Qemu/startup_time/start_vm.sh
@@ -2,4 +2,5 @@
 qemu-system-x86_64 -enable-kvm ./ubuntu-18.04-server-cloudimg-amd64.img  -nographic \
 -cpu host -smp 2 \
 -m 1024 \
--net nic -net tap,ifname=tap1,script=no,downscript=no
+-net nic -net tap
+#  -net tap,ifname=tap1,script=no,downscript=no
